a9bd6507e453864017e1643aa01725fce0d808e7
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2017-08-06 13:25:39 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,18 @@ import { StoreModule } from '@ngrx/store';
 import { environment } from '@environment/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initialState, /* appStateReducer */ } from './app.state';
+import { initialState } from './app.state';
 
 import { CoreModule } from '@core/core.module';
 import * as core from '@core/store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     CoreModule.forRoot(),
 
     // Store
     StoreModule.forRoot({ core: core.reducer }, { initialState }),
     EffectsModule.forRoot([core.CoreEffectsService]),
-
 
     // ISSUE: store-devtools currently causes severe performance problems when used with router-store.
     // We are working to fix this, but for now, avoid using them together.
@@ -33,9 +29,9 @@ import * as core from '@core/store';
     !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [], //  Retains last 50 states
 
     AppRoutingModule,
-
   ],
   providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
