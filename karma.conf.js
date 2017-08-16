719d566faae9c25526a1b2462072e1a68efc7aac
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2017-08-16 10:24:59 +0200

    upgrade to cli 1.3.0 and angular 4.3.4

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -16,9 +16,6 @@ module.exports = function (config) {
     phantomJsLauncher: {
       exitOnResourceError: true
     },
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
     files: [
       // Prevent 'Could not find Angular Material core theme.' warning message
       { pattern: './node_modules/@angular/material/prebuilt-themes/indigo-pink.css', watched: false },
@@ -26,7 +23,6 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
     coverageIstanbulReporter: {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
