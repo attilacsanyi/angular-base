b5d5928fa88913259d288f5616c3d126ca27b86c
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2017-08-16 13:20:31 +0200

    ngrx: upgrade to latest with custom router serialisation support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AngularBase
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.3.0.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { RouterStateSnapshot } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 // Store
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreRouterConnectingModule, routerReducer, RouterStateSerializer } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
@@ -14,23 +15,36 @@ import { initialState } from './app.state';
 import { CoreModule } from '@core/core.module';
 import * as core from '@core/store';
 
+// https://github.com/ngrx/platform/blob/master/docs/router-store/api.md#custom-router-state-serializer
+export interface RouterStateUrl {
+  url: string;
+}
+
+export class CustomRouteStateSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState;
+
+    // Only return an object including the URL
+    // instead of the entire snapshot
+    return { url };
+  }
+}
+
 @NgModule({
   imports: [
     CoreModule.forRoot(),
 
-    // Store
-    StoreModule.forRoot({ core: core.reducer }, { initialState }),
+    // ngrx
+    StoreModule.forRoot({ core: core.reducer, router: routerReducer }, { initialState }),
     EffectsModule.forRoot([core.CoreEffectsService]),
-
-    // ISSUE: store-devtools currently causes severe performance problems when used with router-store.
-    // We are working to fix this, but for now, avoid using them together.
-    // https://github.com/ngrx/platform/issues/97
     StoreRouterConnectingModule,
     !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [], //  Retains last 50 states
 
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: RouterStateSerializer, useClass: CustomRouteStateSerializer }
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
