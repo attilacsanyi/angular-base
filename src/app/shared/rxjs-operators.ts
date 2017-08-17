5a38ed327fe2b19db2f76b2c1d935ff9d06ddae1
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2017-08-17 14:23:39 +0200

    rxjs: add other static operators in comments

--- src/app/shared/rxjs-operators.ts
+++ src/app/shared/rxjs-operators.ts
@@ -6,24 +6,28 @@
 // Statics
 // import 'rxjs/add/observable/throw';
 // import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/of';
+// import 'rxjs/add/observable/forkJoin';
+// import 'rxjs/add/observable/combineLatest';
 
 // Operators
 // import 'rxjs/add/operator/catch';
-// import 'rxjs/add/operator/debounceTime';
 // import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/filter';
-// import 'rxjs/add/operator/single';
 // import 'rxjs/add/operator/delay';
 // import 'rxjs/add/operator/retryWhen';
 // import 'rxjs/add/operator/pluck';
 // import 'rxjs/add/operator/combineLatest';
 // import 'rxjs/add/operator/withLatestFrom';
 import 'rxjs/add/operator/take';
+// import 'rxjs/add/operator/takeWhile';
 // import 'rxjs/add/operator/skip';
 // import 'rxjs/add/operator/concat';
 // import 'rxjs/add/operator/share';
 // import 'rxjs/add/operator/zip';
-import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/do';
 import 'rxjs/observable/defer';
