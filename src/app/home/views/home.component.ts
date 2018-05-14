aaf6cc7522e40a0787ad8062f5d1deaf9ce9fea0
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2018-05-14 22:25:57 +0200

    deps: upgrade angular, material, rxjs, @ngrx, flex-layout to version 6 and other dependencies

--- src/app/home/views/home.component.ts
+++ src/app/home/views/home.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { select, Store } from '@ngrx/store';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 import * as fromStore from '../store';
 
