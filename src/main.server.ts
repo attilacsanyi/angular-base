3d80d23bc14cb24e261c16612d7a9aaebb836949
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2018-05-27 20:11:45 +0200

    universal: ng generate universal angular-base-shell --client-project angular-base

---
+++ src/main.server.ts
@@ -0,0 +1,7 @@
+import { enableProdMode } from '@angular/core';
+
+import { environment } from './environments/environment';
+
+if (environment.production) enableProdMode();
+
+export { AppServerModule } from './app/app.server.module';
