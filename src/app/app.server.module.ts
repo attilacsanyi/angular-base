42733f78595c5f1c36dee7244b01ced381b4aff5
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2018-05-28 17:17:14 +0200

    refactor: use just shell component, tweak npm scripts and shell style

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -5,11 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
-import { AppShellComponent } from './app-shell/app-shell.component';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { ShellComponent } from './shell/shell.component';
 
-const routes: Routes = [{ path: 'shell', component: AppShellComponent }];
+const routes: Routes = [{ path: 'shell', component: ShellComponent }];
 
 /**
  * Universal Rendering
@@ -21,6 +21,6 @@ const routes: Routes = [{ path: 'shell', component: AppShellComponent }];
 @NgModule({
   imports: [AppModule, ServerModule, ModuleMapLoaderModule, FlexLayoutServerModule, RouterModule.forRoot(routes)],
   bootstrap: [AppComponent],
-  declarations: [AppShellComponent]
+  declarations: [ShellComponent]
 })
 export class AppServerModule {}
