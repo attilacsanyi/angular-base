d5272aacdda948c8c5cff346272e751f23cf1fef
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2018-09-24 17:44:43 +0200

    test: tweak jest on circleci

--- jest/jest-global-mocks.ts
+++ jest/jest-global-mocks.ts
@@ -1,5 +1,5 @@
-const mock = () => {
-  let storage: { [key: string]: any } = {};
+const webStorageMock = () => {
+  let storage: Record<string, any> = {};
   return {
     getItem: (key: string) => (key in storage ? storage[key] : null),
     setItem: (key: string, value: any) => (storage[key] = value || ''),
@@ -8,8 +8,8 @@ const mock = () => {
   };
 };
 
-Object.defineProperty(window, 'localStorage', { value: mock() });
-Object.defineProperty(window, 'sessionStorage', { value: mock() });
+Object.defineProperty(window, 'localStorage', { value: webStorageMock() });
+Object.defineProperty(window, 'sessionStorage', { value: webStorageMock() });
 Object.defineProperty(document, 'doctype', { value: '<!DOCTYPE html>' });
 Object.defineProperty(window, 'getComputedStyle', {
   value: () => ({ display: 'none', appearance: ['-webkit-appearance'], getPropertyValue: () => '' })
@@ -20,6 +20,7 @@ Object.defineProperty(window, 'CSS', { value: '' });
 
 /**
  * JSDOM missing transform property when using Angular Material, there is a workaround for it
+ * ISSUE: https://github.com/angular/material2/issues/7101
  * https://github.com/thymikee/jest-preset-angular#the-animation-trigger-transformmenu-has-failed
  */
 Object.defineProperty(document.body.style, 'transform', {
