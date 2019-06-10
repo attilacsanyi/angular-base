8b8f57cfb0dbe4a05ffbee05ecc8e4ddb67e840f
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2019-06-10 23:16:27 +0700

    fix: new reducer function call fix

--- src/app/core/store/reducers/core.reducer.ts
+++ src/app/core/store/reducers/core.reducer.ts
@@ -1,4 +1,4 @@
-import { createReducer, on } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
 
 import { User } from '@core/models';
 import * as actions from '@core/store/actions';
@@ -11,10 +11,14 @@ export const initialState: CoreState = {
   user: undefined
 };
 
-export const reducer = createReducer(
+export const coreReducer = createReducer(
   initialState,
   on(actions.loginSuccess, (state, { user }) => ({ ...state, user })),
   on(actions.logout, () => initialState)
 );
+
+export function reducer(state: CoreState | undefined, action: Action) {
+  return coreReducer(state, action);
+}
 
 export const getUserState = (state: CoreState) => state.user;
